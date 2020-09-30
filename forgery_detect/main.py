e44b2ece33a3f6324bbc2a5e2752fa3865c53bcb
bndeepankan <bn.deepankan@gmail.com>
2020-09-30 11:25:05 +0200

    updated library functions

--- forgery_detect/main.py
+++ forgery_detect/main.py
@@ -6,10 +6,10 @@ Main Code
 """
 
 # example
-image_file = input("Enter image file name:")
+# image_file = input("Enter image file name:")
 
-forgery_detect.detect('../test_images/', image_file, '../output_images/', blockSize=256)
-# path = "/Users/deepankanbn/Downloads/MICC-F220/"
-# for i in glob.glob(path + "*sony_61tamp*.jpg"):
-#     image_file = i.split('/')[-1]
-#     forgery_detect.detect(path, image_file, '../output_images/', blockSize=300)
+# forgery_detect.detect('../test_images/', image_file, '../output_images/', blockSize=256)
+path = "../test_images/"
+for i in glob.glob(path + "*.jpg"):
+    image_file = i.split('/')[-1]
+    forgery_detect.detect(path, image_file, '../output_images/', blockSize=300)
